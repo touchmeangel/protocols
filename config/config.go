450852cfbcaf1f550d95c4db5cc535e542b69b17
touchme <vodkavoid@proton.me>
2026-07-28 12:17:50 +0300

    multiple proxies

--- config/config.go
+++ config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"os"
+	"strings"
+)
+
+type Config struct {
+	Proxies []string
+}
+
+func Load() *Config {
+	cfg := &Config{}
+
+	if proxies := os.Getenv("PROXIES"); proxies != "" {
+		cfg.Proxies = strings.Split(proxies, ",")
+	}
+
+	for i := range cfg.Proxies {
+		cfg.Proxies[i] = strings.TrimSpace(cfg.Proxies[i])
+	}
+
+	return cfg
+}
--- defillama/client.go
+++ defillama/client.go
@@ -2,28 +2,53 @@ package defillama
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"time"
 
 	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpproxy"
 )
 
 type Client struct {
-	client *fasthttp.Client
+	clients []*fasthttp.Client
 }
 
-func New() (*Client, error) {
-	client := &fasthttp.Client{
+func New(proxies []string) (*Client, error) {
+	clients := make([]*fasthttp.Client, 0, len(proxies)+1)
+
+	if len(proxies) == 0 {
+		clients = append(clients, newClient(nil))
+	} else {
+		for _, proxy := range proxies {
+			dialer := fasthttpproxy.FasthttpHTTPDialer(proxy)
+
+			if len(proxy) >= 7 && proxy[:7] == "socks5:" {
+				dialer = fasthttpproxy.FasthttpSocksDialer(proxy)
+			}
+
+			clients = append(clients, newClient(dialer))
+		}
+	}
+
+	return &Client{
+		clients: clients,
+	}, nil
+}
+
+func newClient(dial fasthttp.DialFunc) *fasthttp.Client {
+	return &fasthttp.Client{
 		MaxConnsPerHost:     512,
 		ReadTimeout:         10 * time.Second,
 		WriteTimeout:        10 * time.Second,
 		MaxIdleConnDuration: 90 * time.Second,
-		// Dial: fasthttpproxy.FasthttpSocksDialer("socks5://myuser:mypass@127.0.0.1:1080"),
+		Dial:                dial,
 	}
-
-	return &Client{client: client}, nil
 }
 
-// https://defillama.com/protocol/surf-lending
+func (c *Client) randomClient() *fasthttp.Client {
+	return c.clients[rand.IntN(len(c.clients))]
+}
+
 func (c *Client) get(url string) ([]byte, error) {
 	req := fasthttp.AcquireRequest()
 	resp := fasthttp.AcquireResponse()
@@ -33,7 +58,9 @@ func (c *Client) get(url string) ([]byte, error) {
 	req.SetRequestURI(url)
 	req.Header.SetMethod(fasthttp.MethodGet)
 
-	if err := c.client.DoTimeout(req, resp, 10*time.Second); err != nil {
+	client := c.randomClient()
+
+	if err := client.DoTimeout(req, resp, 10*time.Second); err != nil {
 		return nil, err
 	}
 
