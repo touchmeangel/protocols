fdc72c32c53546d4fd568be56999b505a7c5c30e
touchme <vodkavoid@proton.me>
2026-07-28 14:44:50 +0300

    config load

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/touchmeangel/protocols/config"
 	"github.com/touchmeangel/protocols/defillama"
 )
 
 func main() {
-	client, err := defillama.New()
+	cfg := config.Load()
+
+	client, err := defillama.New(cfg.Proxies)
 	if err != nil {
 		log.Fatal(err)
 	}
