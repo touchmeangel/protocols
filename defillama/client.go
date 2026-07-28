1319b87625cf6e5ac54ea52e01393585d503a5ba
touchme <vodkavoid@proton.me>
2026-07-28 11:43:31 +0300

    +defillama client

--- defillama/client.go
+++ defillama/client.go
@@ -1,10 +1,52 @@
 package defillama
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
 type Client struct {
+	client *fasthttp.Client
 }
 
 func New() (*Client, error) {
-	return &Client{}, nil
+	client := &fasthttp.Client{
+		MaxConnsPerHost:     512,
+		ReadTimeout:         10 * time.Second,
+		WriteTimeout:        10 * time.Second,
+		MaxIdleConnDuration: 90 * time.Second,
+		// Dial: fasthttpproxy.FasthttpSocksDialer("socks5://myuser:mypass@127.0.0.1:1080"),
+	}
+
+	return &Client{client: client}, nil
+}
+
+func (c *Client) get(url string) ([]byte, error) {
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(resp)
+
+	req.SetRequestURI(url)
+	req.Header.SetMethod(fasthttp.MethodGet)
+
+	if err := c.client.DoTimeout(req, resp, 10*time.Second); err != nil {
+		return nil, err
+	}
+
+	body := append([]byte(nil), resp.Body()...)
+	err := statusToError(body, resp.StatusCode())
+	return body, err
+}
+
+func statusToError(body []byte, statusCode int) error {
+	if statusCode != 200 {
+		return fmt.Errorf("request failed with status %d: %s", statusCode, string(body))
+	}
+
+	return nil
 }
 
 func (c *Client) GetAllProtocols() {
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/touchmeangel/protocols
 
 go 1.26.5
+
+require github.com/valyala/fasthttp v1.70.0
+
+require (
+	github.com/andybalholm/brotli v1.2.2 // indirect
+	github.com/klauspost/compress v1.19.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
